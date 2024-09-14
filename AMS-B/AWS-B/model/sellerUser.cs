9d7f18ed3855283fe95586593af353c5606b73e4
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2024-09-14 15:41:56 +0530

    working on sellerUser

--- AMS-B/AWS-B/model/sellerUser.cs
+++ AMS-B/AWS-B/model/sellerUser.cs
@@ -2,5 +2,22 @@ namespace AWS_B.model {
 public class Seller : User
 {
     public override string Role => "Seller";
+    private float seller_rating {get; set;}
+
+    public required List<Car> carList {get; set;} 
+
+    public Seller(){
+        seller_rating = 0.0f;
+        carList = new List<Car>();
+    }
+
+    public List<Car> GetAllCars(){
+    return carList;
+    }
+
+    public void AddCar(Car car){
+        carList.Add(car);
+    } 
+    
 
 }}
