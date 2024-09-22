9f7c98100ec92e293c697bd75c5f110dbb5c60b9
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2024-09-22 17:57:16 +0530

    created publicController

---
+++ AMS-B/Controllers/PublicController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMS_B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+    }
+}
--- AMS-B/obj/Debug/net8.0/AMS-B.AssemblyInfo.cs
+++ AMS-B/obj/Debug/net8.0/AMS-B.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AMS-B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99082cd56aee618daa1894d55822089e912faee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5910e6d41f940b5b9173d8270f555391fd49405e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AMS-B")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AMS-B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
