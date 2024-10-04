f20fde20c4f2ce65770072d56394fe2991f26e58
dilinamewan <dilinamewan07@gmail.com>
2024-10-04 18:33:32 +0530

    buyer done

--- AMS-B/Controllers/BuyerController.cs
+++ AMS-B/Controllers/BuyerController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AMS_B.Models;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AMS_B.Controllers
@@ -7,5 +10,52 @@ namespace AMS_B.Controllers
     [ApiController]
     public class BuyerController : ControllerBase
     {
+        private int GetBuyerId()
+        {
+            var idClaim = User.FindFirst("id");
+            if (idClaim == null)
+            {
+                idClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            }
+
+            return idClaim != null ? int.Parse(idClaim.Value) : 0;
+        }
+
+        [HttpGet("GetActiveAuctions")]
+        public async Task<IActionResult> GetActiveAuctions([FromServices] Dbcon dbcon)
+        {
+            var auctions = await Buyer.GetActiveAuctions(dbcon);
+            return Ok(auctions);
+        }
+
+        [HttpPost("PlaceBid")]
+        public async Task<IActionResult> PlaceBid([FromBody] Bid bid, [FromServices] Dbcon dbcon)
+        {
+            int buyerId = GetBuyerId();
+            if (buyerId <= 0)
+            {
+                return BadRequest(new { Message = "Invalid buyer ID." });
+            }
+
+            bid.UserId = buyerId;
+            bid.BidTime = DateTime.Now;
+
+            var result = await Buyer.PlaceBid(dbcon, bid);
+            if (result)
+            {
+                return Ok(new { Message = "Bid placed successfully." });
+            }
+            else
+            {
+                return BadRequest(new { Message = "Failed to place bid. Please try again." });
+            }
+        }
+
+        [HttpGet("GetBidHistory")]
+        public async Task<IActionResult> GetBidHistory([FromQuery] int auctionId, [FromServices] Dbcon dbcon)
+        {
+            var bidHistory = await Buyer.GetBidHistory(dbcon, auctionId);
+            return Ok(bidHistory);
+        }
     }
 }
