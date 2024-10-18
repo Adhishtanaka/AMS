c3ded8de86756ffaad4e4e271db0a2ff7b795bd2
dilinamewan <dilinamewan07@gmail.com>
2024-10-18 17:54:49 +0530

    updated

--- AMS-B/AWS-B/model/buyerUser.cs
+++
@@ -1,9 +0,0 @@
-namespace AWS_B.model { 
-public class Buyer : User
-{
-    public override string Role => "Buyer";
-     public int BuyerId { get; set; }
-        public string Name { get; set; }
-
-}
-}
