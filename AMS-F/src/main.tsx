ebb34a98374311a92b3b5cecc2601d6c7051f71d
dilinamewan <dilinamewan07@gmail.com>
2024-10-18 18:33:49 +0530

    update

--- AMS-F/src/main.tsx
+++ AMS-F/src/main.tsx
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
+import './index.css';
 import Login from './routes/login';
 import Register from './routes/register';
 import Error from './routes/error';
@@ -16,77 +16,72 @@ import SellerAddCar from './routes/SellerAddCar';
 import SellerCarDetail from './routes/SellerCarDetails';
 import SellerAddAuction from './routes/SellerAddAuction';
 import SellerAuctionDetails from './routes/SellerAuctionDetails';
-import AuctionList from './components/AuctionList';
-import AuctionDetails from './components/AuctionDetails';
+import ActiveAuctions from './components/ActiveAuctions'; // Importing ActiveAuctions component
+import BidForm from './components/BidForm'; // Importing BidForm component
+import BidHistory from './components/BidHistory'; // Importing BidHistory component
 
-
-
-
-const router = createBrowserRouter([{
-  path: "*",
-  element: <Error/>
-},  {
-  path: "/login",
-  element: <Login/>,
-},
-{
-  path: "/Register",
-  element: <Register/>,
-},
-{
-  path: "/Admin",
-  element: <Admin/>,
-},
-{
-  path: "/navbar",
-  element: <Nav/>,
-},
-{
-  path: "/contact",
-  element: <Contact/>,
-},
-{
-  path: "/contact",
-  element: <Contact/>,
-},
-{
-  path: "/seller",
-  element: <SellerDashboard/>,
-},
-{
-  path: "/seller/add-car",
-  element: <SellerAddCar/>,
-},{
-  path: "/seller/car-details/:carId", 
-  element: <SellerCarDetail />,
-},
-{
-  path: "/seller/create-auction",
-  element: <SellerAddAuction/>,
-},
-{
-  path: "/seller/auction-details/:auctionId",
-  element: <SellerAuctionDetails/>,
-},
-{
-  path: "/auctions-list/:auctionId",
-  element: <AuctionList />,
-},
-{
-  path: "/auction-details/:auctionId", // New route for AuctionDetails
-  element: <AuctionDetails />,
-},
-
-
-
-
-])
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/navbar",
+    element: <Nav />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/seller",
+    element: <SellerDashboard />,
+  },
+  {
+    path: "/seller/add-car",
+    element: <SellerAddCar />,
+  },
+  {
+    path: "/seller/car-details/:carId",
+    element: <SellerCarDetail />,
+  },
+  {
+    path: "/seller/create-auction",
+    element: <SellerAddAuction />,
+  },
+  {
+    path: "/seller/auction-details/:auctionId",
+    element: <SellerAuctionDetails />,
+  },
+  {
+    path: "/buyer/active-auctions", // New route for displaying active auctions
+    element: <ActiveAuctions />,
+  },
+  {
+    path: "/buyer/auction/:auctionId/bid", // Route for placing bids
+    element: <BidForm />,
+  },
+  {
+    path: "/buyer/auction/:auctionId/bid-history", // Route for viewing bid history
+    element: <BidHistory />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
-    <>
+  <>
     <RouterProvider router={router} />
     <ToastContainer />
-    </>
-  
-)
+  </>
+);
